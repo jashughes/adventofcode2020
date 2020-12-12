996c252421cc5d36b69a0629e87b8d7eda4175cd
jashughes <jasmine@berkeley.edu>
2020-12-11 23:44:06 -0800

    consolidated pt1 and pt2

---
+++ 11.jl
@@ -0,0 +1,58 @@
+input = split.(readlines("11.txt"), "")
+
+function seatval(input, x, y)
+    if (x) ≤ 0 || (y) ≤ 0 || (x) > length(input[1]) || (y) > length(input)
+        return 0 
+    else
+        return input[y][x]
+    end
+end
+
+function neighbours(input, x, dx, y, dy)
+    seatval(input, x + dx, y + dy) == "#"
+end
+
+function sight_line(input, x, dx, y, dy)
+    sighted = seatval(input, x + dx, y + dy)
+    if sighted == "."
+        return sight_line(input, x + dx, dx, y + dy, dy)
+    else 
+        return sighted == "#"
+    end
+end
+
+function check_seat(input, x, y, decision_maker)
+    coords = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+    sum([decision_maker(input, x, i[1], y, i[2]) for i in coords])
+end
+
+function fill_seats(input, seat_thresh, decision_maker)
+    new_seats = deepcopy(input)
+    for i = 1:length(input[1])
+        for j =1:length(input)
+            if input[j][i] == "#" && check_seat(input, i, j, decision_maker) ≥ seat_thresh
+                new_seats[j][i] = "L"
+            elseif input[j][i] == "L" && check_seat(input, i, j, decision_maker) == 0
+                new_seats[j][i] = "#"
+            end
+        end
+    end
+    return new_seats
+end
+
+function count_filled(outseats)
+    sum([outseats[j][i] == "#" for j in 1:length(outseats), i in 1:length(outseats[1])])
+end
+
+function loop_seats(input, seat_thresh, decision_maker)
+    last_seats = deepcopy(input)
+    filled_seats = fill_seats(input, seat_thresh, decision_maker)
+    while last_seats != filled_seats
+        last_seats = deepcopy(filled_seats)
+        filled_seats = fill_seats(filled_seats, seat_thresh, decision_maker)
+    end
+    return count_filled(filled_seats)
+end
+
+println("Part 1: ", loop_seats(input, 4, neighbours))
+println("Part 2: ", loop_seats(input, 5, sight_line))
