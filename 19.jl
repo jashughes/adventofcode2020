52a4875d1ed64a533630a8a8c2acf80d070d25d0
jashughes <jasmine@berkeley.edu>
2020-12-19 16:51:07 -0800

    tidied day 19

--- 19.jl
+++ 19.jl
@@ -1,175 +1,66 @@
+#= Read Input =#
 rules = replace.(readlines("19_rules.txt"), "\"" => "")
 test = readlines("19_text.txt")
 
-rules = ["0: 4 1 5", "1: 2 3 | 3 2", "2: 4 4 | 5 5", "3: 4 5 | 5 4", "4: a", "5: b"]
-test = ["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"]
-
+#= Useful Functions =#
 function rules_d(rules)
-    Dict(x[1] => split.(split(x[2], " | "), " ") for x in split.(rules, ": "))
+    Dict(x[1] => ifelse(contains(x[2], r"\|"), "( " * x[2] * " )", " " * x[2] * " ") for x in split.(rules, ": "))
 end
-
-function rules_d(rules)
-    Dict(x[1] => "(( " * replace(x[2], " | " => " )|( ") * " ))" for x in split.(rules, ": "))
-end
-
-
-rd["0"]
-start = "0"
 
 function repl_num(find, repl, seg)
-    #seg = replace(seg, Regex("\\($find ") => "(" * repl * " ")
-    #seg = replace(seg, Regex(" $find\\)") => " " * repl * ")")
     replace(seg, Regex(" $find ") => " " * repl * " ")
-end
-
-
-function rev_d(rd, start = "0")
-    end_keys = [k for k in keys(rd) if rd[k] == "(( a ))" || rd[k] == "(( b ))"]
-    known_d = Dict(k => replace(replace(rd[k], "(" => ""), ")" => "") for k in end_keys)
-    delete!(rd, end_keys[1])
-    delete!(rd, end_keys[2])
-    
-    while "0" in keys(rd) && contains(rd[start], r"[0-9]")
-        println("length known ", length(known_d), " rules ", length(rd))
-        for k in keys(known_d)
-            for r in keys(rd)
-                rd[r] = repl_num(k, known_d[k], rd[r])
-                if !contains(rd[r], r"[0-9]")
-                    known_d[r] = rd[r]
-                    delete!(rd, r)
-                end
-            end
-        end
-    end
-    return known_d
-end
-
-function tidy_d(out)
-    # remove extra space, then remove extra brackets
-    for k in keys(out)
-        out[k] = replace(out[k], " " => "")
-    end
-    return out
 end
 
 function match_exp(ru, te)
     replace(te, Regex("$ru") => "")
 end
 
-rd = rules_d(rules)
-out = rev_d(rd, "0")
-out = tidy_d(out)
-println(sum(length(match_exp(out["0"], te)) == 0 for te in test))
-
-
-
-#= Part 2
-=#
-
-function rules_d(rules)
-    Dict(x[1] => ifelse(contains("|", x[2]), "( " * x[2] * " )", x[2]) for x in split.(rules, ": "))
-end
-
-
-function overwrite(rd)
-    rd["8"] = "(( 42 )|( 42 8 ))"
-    rd["11"] = "(( 42 31 )|( 42 11 31 ))"
-end
-
-
-function rev_d(rd)
-    end_keys = [k for k in keys(rd) if rd[k] == "a" || rd[k] == "b"]
-    known_d = Dict(k => replace(replace(rd[k], "(" => ""), ")" => "") for k in end_keys)
+function rev_d(rd, part2 = false)
+    end_keys = [k for k in keys(rd) if rd[k] == " a " || rd[k] == " b "]
+    known = Dict(k => replace(rd[k], "(|)" => "") for k in end_keys)
     delete!(rd, end_keys[1])
     delete!(rd, end_keys[2])
     
-    while length(keys(rd)) > 3
-        println("length known ", length(known_d), " rules ", length(rd))
-        for k in keys(known_d)
-            for r in keys(rd)
-                rd[r] = repl_num(k, known_d[k], rd[r])
-                if !contains(rd[r], r"[0-9]")
-                    known_d[r] = rd[r]
-                    delete!(rd, r)
-                end
+    while length(keys(rd)) > (part2 ? 3 : 0)
+        for k in keys(known), r in keys(rd)
+            rd[r] = repl_num(k, known[k], rd[r])
+            if !contains(rd[r], r"[0-9]")
+                known[r] = rd[r]
+                delete!(rd, r)
             end
         end
     end
-    return known_d
+    return Dict(k => replace(known[k], " " => "") for k in keys(known))
 end
 
-rules = replace.(readlines("19_rules.txt"), "\"" => "")
-test = readlines("19_text.txt")
-
-rd = rules_d(rules)
-overwrite(rd)
-partial_d = tidy_d(rev_d(rd))
-zero_rules = "811"
-zero_rules = replace8(zero_rules, partial_d["42"])
-#zero_rules = replace11(zero_rules, partial_d["42"], partial_d["31"])
-
-println(sum(length(match_exp(zero_rules, te)) == 0 for te in test))
-
-rd["8"]
-rd["11"]
-rd["0"]
-partial_d["42"]
-partial_d["31"]
-
-function tidy_brackets(seg)
-    nc = findfirst(r"\)[^|]", seg)
-    if nc == nothing
-        return seg
-    else
-        nc = nc[1]
-    end
-    fb = maximum(only.(findall(r"\(", seg[1:nc])))
-    seg[1:(fb - 1)] * seg[(fb + 1):(nc - 1)] * seg[(nc + 1):end]
-end
-
-function keep_cleaning(seg)
-    nch = length(seg)
-    new_seg = tidy_brackets(seg)
-    while length(new_seg) < nch
-        nch = length(new_seg)
-        new_seg = tidy_brackets(new_seg)
-    end
-    return new_seg
-end
-
-
-
-function replace8(seg, fortytwo)
+function rule8(seg, fortytwo)
     replace(seg, "8" => "(" * fortytwo * ")+")
 end
 
 
-function replace11(seg, fortytwo, thirtyone, x)
+function rule11(seg, fortytwo, thirtyone, x)
     fancified = "(" * fortytwo * "){" * string(x) * "}(" * thirtyone * "){" * string(x) * "}"
     replace(seg, "11" => fancified)
 end
 
-function loop11(partial_d, rd)
-    zero_rules = "811"
-    fortytwo = keep_cleaning(partial_d["42"])
-    thirtyone = keep_cleaning(partial_d["31"])
-    zero_rules = replace8(zero_rules, fortytwo)
-    s = 0
-
-    for te in test
-        if any(length(match_exp(replace11(zero_rules, fortytwo, thirtyone, x), te)) == 0 for x in 1:44)
-            s+= 1
-        end
-    end
-    return s
+function rule0(d)
+    #=  This function hardcodes in the new specifications for Rule 0 ("8 11")
+        Then, we replace the regex for Rule 8 and Rule 11:
+            Rule 8: r"(42)+"
+            Rule 11: r"(42){N}(31){N}"
+        The theoretical maximum value for N is [(max N char) - 1]/2, but that hits the max regex
+        length in julia. Empirically, increasing N beyond 4 does not impact the number of matches,
+        so for this solution, testing N = 1:4 is enough! =#
+    zero_rules = rule8("811", d["42"])
+    sum(any(length(match_exp(rule11(zero_rules, d["42"], d["31"], N), te)) == 0 for N = 1:4) for te in test)
 end
 
-println(loop11(partial_d, rd))
+#= Part 1 =#
+rd = rules_d(rules)
+out = rev_d(rd, false)
+println("Part 1: ", sum(length(match_exp(out["0"], te)) == 0 for te in test))
 
-
-
-
-count_char("\\)", zero_rules)
-
+#= Part 2 =#
+println("Part 2: ", rule0(out))
 
 
